c789a2d4bd5306cedb0cd5749c1ed4e426d64f0f
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2021-10-10 12:47:11 -0600

    consumo de api para cargado de datos en el dom o mostrar error en caso de existir uno al consumir api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import axios, {Axios} from 'axios';
+import React, {useEffect, useReducer} from "react";
+
+const apiURL = 'https://jsonplaceholder.typicode.com/todos';
+
+const initialState = {
+    loading: true,
+    error: '',
+    todos: []
+};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_DATA':
+            return {
+                loading: false,
+                error: '',
+                todos: action.payload
+            };
+        case 'SET_ERROR':
+            return {
+                loading: false,
+                error: 'There are some errors',
+                todos: []
+            }
+    }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [state, dispatch] = useReducer(reducer, initialState);
+    useEffect(() => {
+        axios.get(apiURL)
+            .then(response => {
+                dispatch({type: 'SET_DATA', payload: response.data});
+            })
+            .catch(error => {
+                dispatch({type: 'SET_ERROR'});
+            })
+    }, []);
+
+    const listMarkup = (<ul>
+        {state.todos.map(todo => (<li key={todo.id}>{todo.title}</li>))}
+    </ul>);
+
+    return (
+        <div className="App">
+            {state.loading ? 'Loading' : state.error ? state.error : listMarkup}
+        </div>
+    );
 }
 
 export default App;
